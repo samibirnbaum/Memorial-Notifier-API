9db23367aeca2ef27508a32556e9f5064ca72866
SAMI BIRNBAUM <sami_birnbaum@btconnect.com>
2018-05-04 11:26:00 +0100

    create user over api complete

---
+++ app/controllers/api/registrations_controller.rb
@@ -0,0 +1,26 @@
+class Api::RegistrationsController < Devise::RegistrationsController  
+    respond_to :json
+
+    def create
+        build_resource(sign_up_params)
+    
+        resource.save
+        yield resource if block_given?
+        if resource.persisted?
+          if resource.active_for_authentication?
+            # set_flash_message! :notice, :signed_up
+            # sign_up(resource_name, resource) - taken out to stop user being auto logged in after sign up
+            respond_with resource, location: after_sign_up_path_for(resource)
+          else
+            set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+            expire_data_after_sign_in!
+            respond_with resource, location: after_inactive_sign_up_path_for(resource)
+          end
+        else
+          clean_up_passwords resource
+          set_minimum_password_length
+          respond_with resource
+        end
+    end
+    
+end  
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,25 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
-    devise_for :users
+  scope :api, defaults: { format: :json } do
+    devise_for :users, :controllers => {sessions: 'sessions', registrations: 'api/registrations'} 
   end
 end
 
-# Prefix                         Verb     URI Pattern                                  Controller#Action
-# new_api_user_session           GET      /api/users/sign_in(.:format)                 api/sessions#new {:format=>:json}                                                            
-# api_user_session               POST     /api/users/sign_in(.:format)                 api/sessions#create {:format=>:json}
-# destroy_api_user_session       DELETE   /api/users/sign_out(.:format)                api/sessions#destroy {:format=>:json}
 
-# new_api_user_password          GET      /api/users/password/new(.:format)            api/passwords#new {:format=>:json}
-# edit_api_user_password         GET      /api/users/password/edit(.:format)           api/passwords#edit {:format=>:json}
-#    api_user_password           PATCH    /api/users/password(.:format)                api/passwords#update {:format=>:json}son}                                                                                                                                                     
-#                                PUT      /api/users/password(.:format)                api/passwords#update {:format=>:json}son}                                                                                                                                                       
-#                                POST     /api/users/password(.:format)                api/passwords#create {:format=>:jjson}son}                                                                                                                                                      
+#     Prefix                Verb      URI Pattern                                 Controller#Action
+# new_user_session          GET       /api/users/sign_in(.:format)                sessions#new {:format=>:json}            
+# user_session              POST      /api/users/sign_in(.:format)                sessions#create {:format=>:json}
+# destroy_user_session      DELETE    /api/users/sign_out(.:format)               sessions#destroy {:format=>:json}
 
-# cancel_api_user_registration   GET      /api/users/cancel(.:format)                  api/registrations#cancel {:format=>:json}                                                                                                                                                   
-# new_api_user_registration      GET      /api/users/sign_up(.:format)                 api/registrations#new {:format=>:t=>:json}                                                                                                                                                        
-# edit_api_user_registration     GET      /api/users/edit(.:format)                    api/registrations#edit {:format=>:json}                                                                                                                                                        
-# api_user_registration          PATCH    /api/users(.:format)                         api/registrations#update {:format=>:json}
-#                                PUT      /api/users(.:format)                         api/registrations#update {:format=>:json}                                                                             
-#                                DELETE   /api/users(.:format)                         api/registrations#destroy {:format=>:json}
-#                                POST     /api/users(.:format)                         api/registrations#create {:format=>:json}                                                                                               
+# new_user_password         GET       /api/users/password/new(.:format)           devise/passwords#new {:format=>:json}
+# edit_user_password        GET       /api/users/password/edit(.:format)          devise/passwords#edit {:format=>:json}            
+# user_password             PATCH     /api/users/password(.:format)               devise/passwords#update {:format=>:json}    
+#                           PUT       /api/users/password(.:format)               devise/passwords#update {:format=>:json}
+#                           POST      /api/users/password(.:format)               devise/passwords#create {:format=>:json}
+
+# cancel_user_registration  GET       /api/users/cancel(.:format)                 api/registrations#cancel {:format=>:json}
+#     new_user_registration GET       /api/users/sign_up(.:format)                api/registrations#new {:format=>:json}
+#    edit_user_registration GET       /api/users/edit(.:format)                   api/registrations#edit {:format=>:json}        
+#    user_registration      PATCH     /api/users(.:format)                        api/registrations#update {:format=>:json}
+#                           PUT       /api/users(.:format)                        api/registrations#update {:format=>:json}                          
+#                           DELETE    /api/users(.:format)                        api/registrations#destroy {:format=>:json}
+#                           POST      /api/users(.:format)                        api/registrations#create {:format=>:json}
